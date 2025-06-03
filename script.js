702295750ae4c5450ba54060e25732db48584870
Ark0333 <hnkd.m24@icloud.com>
2025-06-04 00:01:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -99,3 +99,11 @@ function showResult(resultType) {
   resultElement.classList.remove("hidden"); // 非表示を解除
   resultElement.classList.add("fade-in"); // フェードイン演出
 }
+// 結果表示のところ（例）
+function showResult(resultType) {
+  const resultElement = document.getElementById("result");
+  
+  resultElement.innerHTML = "診断結果の内容をここに表示！";
+  resultElement.classList.remove("hidden"); // 非表示を解除
+  resultElement.classList.add("fade-in"); // フェードイン演出
+}
